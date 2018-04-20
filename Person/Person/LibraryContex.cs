09733400c4fc9ce278d8323d764fce07f29efd4d
neffati <aimen.neffati@gmail.com>
2018-04-20 23:57:24 +0200

    essaie code

---
+++ Person/Person/LibraryContex.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    class LibraryContex : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Person> Persons { get; set; }
+    }
+}
---
+++ Person/Person/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    class Person
+    {
+        public int Id { get; set; }
+        public string Produit { get; set; }
+        public string Client { get; set; }
+        public string Fournisseur { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+
+    public class LibraryContect : DbContext
+    {
+        public DBSet<Book> Books { get; set; }
+        public DBSet<Person> Persons { get; set; }
+    }
+}
